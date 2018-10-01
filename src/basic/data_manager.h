1b6eba2620d9961b363c91ed5b44ac435b415f4a
root <chncwang@gmail.com>
2018-10-01 09:45:26 +0800

    some change

--- src/basic/data_manager.h
+++ src/basic/data_manager.h
@@ -5,13 +5,20 @@
 #include <fstream>
 #include <iostream>
 #include "conversation_structure.h"
+#include <boost/algorithm/string.hpp>
 
 std::vector<PostAndResponses> readPostAndResponses(
         const std::string &filename) {
     std::vector<PostAndResponses> results;
     std::string line;
-    while (std::getline(std::ifstream(filename), line)) {
-        std::cout << line << std::endl;
+    std::ifstream ifs(filename);
+    while (std::getline(ifs, line)) {
+        vector<string> strs;
+        boost::split(strs, line, boost::is_any_of(":"));
+        if (strs.size() != 2) {
+            abort();
+        }
+        std::cout << strs.at(0) << " " << strs.at(1) << std::endl;
     }
 
     return results;
--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,17 @@
 #include "data_manager.h"
 #include "cxxopts.hpp"
 
+using namespace std;
 using namespace cxxopts;
 
 int main(int argc, char *argv[]) {
     Options options("single-turn-conversation", "single turn conversation");
-    options.add_options();
-    
+    options.add_options()
+        ("p,pair", "pair file name", cxxopts::value<string>());
+    auto args = options.parse(argc, argv);
+    string pair_filename = args["pair"].as<string>();
+
+    readPostAndResponses(pair_filename);
+
     return 0;
 }
