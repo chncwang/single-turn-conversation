8a1c66d2f55700c07b8c2c99e20fbb12a2f8e275
Chauncey Wang <chncwang@gmail.com>
2019-03-09 15:22:47 +0800

    fix bugs

--- src/single_turn_conversation/default_config.h
+++ src/single_turn_conversation/default_config.h
@@ -20,6 +20,7 @@ struct DefaultConfig {
     bool one_response;
     bool learn_test;
     bool save_model_per_batch;
+    bool split_unknown_words;
     int max_sample_count;
     int dev_size;
     int test_size;
@@ -38,6 +39,7 @@ struct DefaultConfig {
             << "one_response:" << one_response << std::endl
             << "learn_test:" << learn_test << std::endl
             << "save_model_per_batch:" << save_model_per_batch << std::endl
+            << "split_unknown_words:" << split_unknown_words << std::endl
             << "max_sample_count:" << max_sample_count << std::endl
             << "dev_size:" << dev_size << std::endl
             << "test_size:" << test_size << std::endl
